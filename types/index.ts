fb09152ffebb6fe2d6cde76070913f5b30607301
Md. Nesad <131846011+Md-Nesad@users.noreply.github.com>
2025-04-27 02:35:16 +0600

    Created checkout page with advance functionality

--- types/index.ts
+++ types/index.ts
@@ -2,6 +2,7 @@ import {
   CartSchema,
   OrderItemSchema,
   ProductInputSchema,
+  ShippingAddressSchema,
   UserInputSchema,
   UserSignInSchema,
   UserSignUpSchema,
@@ -28,7 +29,7 @@ export type Data = {
 // add to cart
 export type OrderItem = z.infer<typeof OrderItemSchema>
 export type Cart = z.infer<typeof CartSchema>
-
+export type ShippingAddress = z.infer<typeof ShippingAddressSchema>
 // user
 export type IUserInput = z.infer<typeof UserInputSchema>
 export type IUserSignIn = z.infer<typeof UserSignInSchema>
